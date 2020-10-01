0e27361c4fb7024f680946ac7d4941eeb5c988ae
Daniel Nicolae <DeadlyPoet@yandex.com>
2020-10-01 16:17:53 +0300

    Procfile

--- MyWorks/settings.py
+++ MyWorks/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0k#lq+%#uvgivj@bd3^@x2g)o_fijjx9u8wdrg^#3x=h!u3=w4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deadly-poet.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MyWorks.wsgi --log-file -
