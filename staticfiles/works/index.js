0488475c9b1785b50589c126241f06a6f97e7f41
DanielNicolae <deadlypoet@protonmail.com>
2022-01-08 15:27:47 +0200

    I added a transform and a transition for the cards, on both hover and focus-within.

--- staticfiles/works/index.js
+++ staticfiles/works/index.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('#games').addEventListener('click', ()=>loadGames());
-    document.querySelector('#models').addEventListener('click', ()=>loadModels());
-    loadGames();
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#games').addEventListener('click', () => loadGames());
+  document.querySelector('#models').addEventListener('click', () => loadModels());
+  loadGames();
 });
 
 function loadGames() {
-    document.querySelector('#allgames').style.display = 'block';
-    document.querySelector('#allmodels').style.display = 'none';
+  document.querySelector('#allgames').style.display = 'block';
+  document.querySelector('#allmodels').style.display = 'none';
 }
 
 function loadModels() {
-    document.querySelector('#allgames').style.display = 'none';
-    document.querySelector('#allmodels').style.display = 'block';
+  document.querySelector('#allgames').style.display = 'none';
+  document.querySelector('#allmodels').style.display = 'block';
 }
